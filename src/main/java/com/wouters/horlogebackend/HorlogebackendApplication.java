eb6e00ded2650232a3beaff9fa7d579674a66c3e
ThierryWouters <thierry@thierrywouters.com>
2022-07-31 10:54:46 +0200

    start

---
+++ src/main/java/com/wouters/horlogebackend/HorlogebackendApplication.java
@@ -0,0 +1,13 @@
+package com.wouters.horlogebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorlogebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorlogebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wouters/horlogebackend/HorlogebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wouters.horlogebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorlogebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
